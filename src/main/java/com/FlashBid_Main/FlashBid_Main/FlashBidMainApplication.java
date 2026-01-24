71f83d19847c650a61451f232993031a077ba5e0
junyeong <whwnsdud4592@naver.com>
2026-01-24 17:18:16 +0900

    first commit

---
+++ src/main/java/com/FlashBid_Main/FlashBid_Main/FlashBidMainApplication.java
@@ -0,0 +1,13 @@
+package com.FlashBid_Main.FlashBid_Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashBidMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashBidMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlashBid_Main/FlashBid_Main/FlashBidMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlashBid_Main.FlashBid_Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashBidMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
