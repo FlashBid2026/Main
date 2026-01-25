b6d7ea86d9d12602b2a866dd58fe7f60b59dd03d
junyeong <whwnsdud4592@naver.com>
2026-01-25 17:44:50 +0900

    feat: Add HomeCotroller

---
+++ src/main/java/com/FlashBid_Main/FlashBid_Main/Home/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.FlashBid_Main.FlashBid_Main.Home.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String homePage(){
+
+    return "home";
+  }
+}
