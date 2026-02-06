82da71de8ba91eff0e8ec8bcadd984983f69fc4b
junyeong <whwnsdud4592@naver.com>
2026-02-07 01:17:25 +0900

    feat: 입찰기록 테이블

---
+++ src/main/java/com/FlashBid_Main/FlashBid_Main/Item/domain/Bid.java
@@ -0,0 +1,42 @@
+package com.FlashBid_Main.FlashBid_Main.Item.domain;
+
+import com.FlashBid_Main.FlashBid_Main.Auth.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bid {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id", nullable = false)
+  private Item item;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private Long bidPrice;
+
+  @Column(nullable = false)
+  private LocalDateTime bidAt;
+
+  @Builder
+  public Bid(Item item, User user, Long bidPrice) {
+    this.item = item;
+    this.user = user;
+    this.bidPrice = bidPrice;
+    this.bidAt = LocalDateTime.now();
+  }
+}
